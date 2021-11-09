ac5ff7de8903e96163d67f0c38aa6874290b2ade
Andrew M. Bradley <ambrad@cs.stanford.edu>
2021-11-09 15:17:32 -0700

    use packs

--- nested_loops/cke_impl1.cpp
+++ nested_loops/cke_impl1.cpp
@@ -5,29 +5,22 @@
 
 namespace cke {
 
-void qad (const Data& d) {
-  const auto tracerCur = scalarize(d.tracerCur);
-  const auto cellMask = scalarize(d.cellMask);
-  const auto normalThicknessFlux = scalarize(d.normalThicknessFlux);
-  const auto advMaskHighOrder = scalarize(d.advMaskHighOrder);
-  const auto highOrderFlx = scalarize(d.highOrderFlx);
+void run (const Data& d) {
+  const auto nvlpk = d.highOrderFlx.extent_int(1);
   for (int iEdge = 0; iEdge < d.nEdges; ++iEdge)
-    for (int k = 0; k < d.nVertLevels; ++k) {
-      const auto coef2 = normalThicknessFlux(iEdge,k)*advMaskHighOrder(iEdge,k);
-      const auto csgn = normalThicknessFlux(iEdge,k) < 0 ? -1 : 1;
-      Real edgeFlx = 0;
+    for (int k = 0; k < nvlpk; ++k) {
+      const auto coef2 = d.normalThicknessFlux(iEdge,k)*d.advMaskHighOrder(iEdge,k);
+      Data::Pr csgn(1);
+      csgn.set(d.normalThicknessFlux(iEdge,k) < 0, -1);
+      Data::Pr edgeFlx = 0;
       for (int i = 0; i < d.nAdvCellsForEdge(iEdge); ++i) {
         const auto iCell = d.advCellsForEdge(iEdge,i);
-        // not necessary: if (k < d.minLevelCell(iCell) || k > d.maxLevelCell(iCell)) continue;
         const auto coef1 = d.advCoefs(iEdge,i);
         const auto coef3 = d.advCoefs3rd(iEdge,i)*d.coef3rdOrder;
-        edgeFlx += tracerCur(iCell,k)*cellMask(iCell,k)*coef2*(coef1 + coef3*csgn);
+        edgeFlx += d.tracerCur(iCell,k)*d.cellMask(iCell,k)*coef2*(coef1 + coef3*csgn);
       }
-      highOrderFlx(iEdge,k) = edgeFlx;
+      d.highOrderFlx(iEdge,k) = edgeFlx;
     }
-}
-
-void run (const Data& d) {
 }
 
 } // namespace cke
@@ -35,7 +28,6 @@ void run (const Data& d) {
 void cke_impl1_run () {
   const auto d = cke::get_Data_singleton();
   assert(d);
-  cke::qad(*d);
   for (int iter = 0; iter < d->nIters; ++iter)
     cke::run(*d);
 }
