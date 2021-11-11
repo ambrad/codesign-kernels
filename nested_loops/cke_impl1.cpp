6c32a9f0278247c634fa4defdfda7823d99680b7
Andrew M. Bradley <ambrad@cs.stanford.edu>
2021-11-10 18:19:19 -0700

    rm cpuref

--- nested_loops/cke_impl1.cpp
+++ nested_loops/cke_impl1.cpp
@@ -5,36 +5,6 @@
 
 namespace cke {
 
-void cpuref (const Data& d) {
-  static constexpr int max_nvl = 128;
-  const auto nvlpk = ekat::PackInfo<Data::packn>::num_packs(d.nVertLevels);
-  const auto normalThicknessFlux_s = scalarize(d.normalThicknessFlux);
-  for (int iEdge = 0; iEdge < d.nEdges; ++iEdge) {
-    Data::Pr coef2[max_nvl/Data::packn];
-    for (int k = 0; k < nvlpk; ++k)
-      coef2[k] = d.normalThicknessFlux(iEdge,k)*d.advMaskHighOrder(iEdge,k);
-    Data::Pr csgn[max_nvl/Data::packn];
-    auto* const scsgn = reinterpret_cast<Real*>(&csgn[0]);
-    vector_simd for (int k = 0; k < d.nVertLevels; ++k)
-      scsgn[k] = normalThicknessFlux_s(iEdge,k) < 0 ? -1 : 1;
-    Data::Pr edgeFlx[max_nvl/Data::packn];
-    for (int k = 0; k < nvlpk; ++k)
-      edgeFlx[k] = 0;
-    const auto iend = d.nAdvCellsForEdge(iEdge);
-    for (int i = 0; i < iend; ++i) {
-      const auto coef1 = d.advCoefs(iEdge,i);
-      const auto coef3 = d.advCoefs3rd(iEdge,i)*d.coef3rdOrder;
-      const auto iCell = d.advCellsForEdge(iEdge,i);
-      const auto kbeg = d.minLevelCell(iCell)/Data::packn;
-      const auto kend = d.maxLevelCell(iCell)/Data::packn;
-      for (int k = kbeg; k <= kend; ++k)
-        edgeFlx[k] += d.tracerCur(iCell,k)*d.cellMask(iCell,k)*coef2[k]*(coef1 + coef3*csgn[k]);
-    }
-    for (int k = 0; k < nvlpk; ++k)
-      d.highOrderFlx(iEdge,k) = edgeFlx[k];
-  }
-}
-
 struct NestedLoopKernel {
   struct Data d;
   NestedLoopKernel (const Data& d_) : d(d_) {}
